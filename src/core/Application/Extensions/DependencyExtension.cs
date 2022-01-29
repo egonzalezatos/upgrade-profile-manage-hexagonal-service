37f6c0b91cb62c578c0d89e74c7a7404e04bf802
kurayamigin <edwin-julian.gonzalez@atos.net>
2022-01-28 18:17:21 -0600

    [UPGRADE] Eliminando ConfigDB no usado y renombrando clase estatica en app.

--- src/core/Application/Extensions/DependencyExtension.cs
+++ src/core/Application/Extensions/DependencyExtension.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Application.Extensions
 {
-    public static class ServiceDiExtension
+    public static class DependencyExtension
     {
         public static void AddApplication(this IServiceCollection services)
         {
