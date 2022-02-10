06bb761b53bcfccde5d76d1eec47e678be145192
kurayamigin <edwin-julian.gonzalez@atos.net>
2022-02-10 14:18:41 -0600

    seeding data

---
+++ src/Infrastructure/Migrations/20220210185927_egonzalezatos_seeding_data.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+public partial class egonzalezatos_seeding_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Status",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, null, "Active" },
+                    { 2, null, "Disabled" },
+                    { 3, null, "Pending" },
+                    { 4, null, "Warning" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
